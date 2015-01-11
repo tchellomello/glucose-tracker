bf848a591d6c8a0227d379bc72f410e422a903a5
jonathan <jonathan@calazan.com>
2015-01-11 15:04:17 -0500

    Updated admin fields, updated screenshots.

--- glucosetracker/glucoses/admin.py
+++ glucosetracker/glucoses/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.text import Truncator
 
 from .models import Glucose, Category
 
@@ -9,7 +10,8 @@ class GlucoseAdmin(admin.ModelAdmin):
         'category',
         'record_date',
         'record_time',
-        'notes',
+        'notes_truncated',
+        'tag_list',
         'user',
         'created',
         'modified',
@@ -19,6 +21,18 @@ class GlucoseAdmin(admin.ModelAdmin):
         'user',
         'category',
     ]
+
+    def notes_truncated(self, obj):
+        return Truncator(obj.notes).chars(75)
+    notes_truncated.admin_order_field = 'notes'
+    notes_truncated.short_description = 'Notes'
+
+    def tag_list(self, obj):
+        """
+        Retrieve the tags separated by comma.
+        """
+        return ', '.join([t.name for t in obj.tags.all()])
+    tag_list.short_description = 'Tags'
 
 
 class CategoryAdmin(admin.ModelAdmin):
